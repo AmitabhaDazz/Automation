75fc9dd21328262273e48e17fd1698d29c21fff1
Tanusree Chakraborty <Tanusree Chakraborty@TANUSREE_CHAKRABORTY>
2019-04-08 15:26:34 +0530

    conflict

--- ReconAutomation/src/main/java/Recon/ReconAutomation/cmp/CmpFilePojo.java
+++ ReconAutomation/src/main/java/Recon/ReconAutomation/cmp/CmpFilePojo.java
@@ -1,7 +1,20 @@
-package Recon.ReconAutomation.cmp;
-
-public class CmpFilePojo {
-	
-
-
-}
+/*
+ * package Recon.ReconAutomation.cmp;
+ * 
+ * public class CmpFilePojo {
+ * 
+ * String resellerCompanyName; String subscriptionId; String partnerCost; String
+ * resellerCost; public String getResellerCompanyName() { return
+ * resellerCompanyName; } public void setResellerCompanyName(String
+ * resellerCompanyName) { this.resellerCompanyName = resellerCompanyName; }
+ * public String getSubscriptionId() { return subscriptionId; } public void
+ * setSubscriptionId(String subscriptionId) { this.subscriptionId =
+ * subscriptionId; } public String getPartnerCost() { return partnerCost; }
+ * public void setPartnerCost(String partnerCost) { this.partnerCost =
+ * partnerCost; } public String getResellerCost() { return resellerCost; }
+ * public void setResellerCost(String resellerCost) { this.resellerCost =
+ * resellerCost; }
+ * 
+ * 
+ * }
+ */

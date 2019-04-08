b8f9fb0a6a70e1f6775b8c04e13599211ed9a2ee
Tanusree Chakraborty <Tanusree Chakraborty@TANUSREE_CHAKRABORTY>
2019-04-08 12:44:58 +0530

    pojo

--- ReconAutomation/src/main/java/Recon/ReconAutomation/raw/RawFilePojo.java
+++ ReconAutomation/src/main/java/Recon/ReconAutomation/raw/RawFilePojo.java
@@ -1,5 +1,24 @@
 package Recon.ReconAutomation.raw;
 
 public class RawFilePojo {
-
+	
+	    
+	    private String subscriptionid;
+	    private String posttaxtotal;
+		public String getSubscriptionid() {
+			return subscriptionid;
+		}
+		public void setSubscriptionid(String subscriptionid) {
+			this.subscriptionid = subscriptionid;
+		}
+		public String getPosttaxtotal() {
+			return posttaxtotal;
+		}
+		public void setPosttaxtotal(String posttaxtotal) {
+			this.posttaxtotal = posttaxtotal;
+		}
+	    
+        return " firstName=" + firstName
+            + ", lastName=" + lastName + ", salary=" + salary + "]";
+}
 }

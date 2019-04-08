5ba747cc6fc6f3acb347e9966207971d718dc264
Tanusree Chakraborty <Tanusree Chakraborty@TANUSREE_CHAKRABORTY>
2019-04-08 12:59:46 +0530

    rawpojo problem

--- ReconAutomation/src/main/java/Recon/ReconAutomation/raw/RawFilePojo.java
+++ ReconAutomation/src/main/java/Recon/ReconAutomation/raw/RawFilePojo.java
@@ -5,21 +5,6 @@ public class RawFilePojo {
 	    
 	    private String subscriptionid;
 	    private String posttaxtotal;
-		public String getSubscriptionid() {
-			return subscriptionid;
-		}
-		public void setSubscriptionid(String subscriptionid) {
-			this.subscriptionid = subscriptionid;
-		}
-		public String getPosttaxtotal() {
-			return posttaxtotal;
-		}
-		public void setPosttaxtotal(String posttaxtotal) {
-			this.posttaxtotal = posttaxtotal;
 		
-		}
-		return "RawFileProcessing [subscriptionid=" + subscriptionid + ", posttaxtotal=" + posttaxtotal
-                + "]";
-}
 }
 

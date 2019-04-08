5cc8cff37d4e42862b332accbf73658cbc2f0757
AmitabhaDazz <Amitabha Das@AMITABHA2_DAS>
2019-04-08 15:51:36 +0530

    startAuto

--- ReconAutomation/src/main/java/Recon/ReconAutomation/StartAuto.java
+++ ReconAutomation/src/main/java/Recon/ReconAutomation/StartAuto.java
@@ -7,11 +7,6 @@ import Recon.ReconAutomation.cmp.CmpFileProcessing;
 import Recon.ReconAutomation.raw.RawFileProcessing;
 
 public class StartAuto {
-
-
-
-	 
-
 	public static void main(String[] args) throws IOException {
 
 		String url = "E:\\New folder\\testx.csv";
@@ -20,34 +15,7 @@ public class StartAuto {
 		CmpFileProcessing cmp = new CmpFileProcessing();
 		cmp.readFile(urlcmp);
 		System.out.println("The directory to read the RAW files is - : " + url);
-
-		/*
-		 *  CmpFileProcessing cmpFileProcessing = new CmpFileProcessing(url);
-		 * 
-		 * 
-		 * cmpFileProcessing.readFile(url);
-		 */
-
-	/*System.out.println("Keep Calm and Code");
-=======
-		String url = "E:\\New folder\\a.xlsm";
-		System.out.println("The directory to read the RAW files is - : " + url); 
->>>>>>> branch 'master' of https://github.com/AmitabhaDazz/Automation.git
-		//System.out.println("The directory to read the CMP files is - : " + args[1]);
-		/*
-		 * System.out.println("The Country name is - : " + args[2]);
-		 * System.out.println("The Continent - : " + args[3]);
-		 */
-	/*	
-		CmpFileProcessing cmpFileProcessing = new CmpFileProcessing(url);
-		/* cmpFileProcessing.readFile(new File(args[0]), args[2], args[3]); */
-		//String path= args[0];
-		//cmpFileProcessing.readFile(url); 
-		
-
 		RawFileProcessing rawFileProcessing = new RawFileProcessing(url);
-		/* cmpFileProcessing.readFile(new File(args[0]), args[2], args[3]); */
-		//String path= args[0];
 		rawFileProcessing.readFile(url); 
 		System.out.println("Keep Calm and Code");
 

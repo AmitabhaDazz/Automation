da55259506cb596209bdf4a935e429c58a12de70
Tanusree Chakraborty <Tanusree Chakraborty@TANUSREE_CHAKRABORTY>
2019-04-09 16:07:51 +0530

    raw done

--- ReconAutomation/src/main/java/Recon/ReconAutomation/raw/RawFileProcessed.java
+++ ReconAutomation/src/main/java/Recon/ReconAutomation/raw/RawFileProcessed.java
@@ -1,5 +1,82 @@
 package Recon.ReconAutomation.raw;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.sun.xml.internal.bind.v2.runtime.reflect.ListIterator;
+
+
 public class RawFileProcessed {
 
+	private List<RawFilePojo> rawList;
+   Map<String, List<RawFilePojo>> subscriptionidMap = new HashMap<String, List<RawFilePojo>>();
+   public RawFileProcessed(List<RawFilePojo> cmpList) {
+
+	this.rawList = rawList;
+    ListIterator<RawFilePojo> iterator = (ListIterator<RawFilePojo>) rawList.listIterator();
+
+	String subscriptionID = "";
+
+	String resellerCost = "";
+
+	String posttaxtotalCost = "";
+
+	List<RawFilePojo> localListperSubscriptionID;
+
+	while (iterator.hasNext()) {
+
+	RawFilePojo objj = (RawFilePojo)iterator.next();
+
+	subscriptionID = objj.getSubscriptionid();
+
+	resellerCost = objj.getResellerCost();
+
+	if(subscriptionidMap.containsKey(subscriptionID)) {
+
+	localListperSubscriptionID = subscriptionidMap.get(subscriptionID);
+
+	localListperSubscriptionID.add(objj);
+
+	subscriptionidMap.put(subscriptionID, localListperSubscriptionID);
+
+	}else {
+
+	localListperSubscriptionID = new ArrayList<RawFilePojo>();
+    localListperSubscriptionID.add(objj);
+    subscriptionidMap.put(subscriptionID, localListperSubscriptionID);
+	}
+       } 
+	      }
+
+public Map<String, List<RawFilePojo>> getSubscriptionIDMap() {
+return subscriptionidMap;
 }
+    public void setSubscriptionIDMap(Map<String, List<RawFilePojo>> subscriptionIDMap) {
+     this.subscriptionidMap = subscriptionIDMap;
+}
+public Map<String, List<RawFilePojo>> getAllSubscriptionIDTotal(){
+return subscriptionidMap;
+}
+	public String getSubscriptionIDTotalResellerCost(String subscriptionID){
+    List<RawFilePojo> subTotal = subscriptionidMap.get(subscriptionID);
+    ListIterator<RawFilePojo> iterator = (ListIterator<RawFilePojo>) subTotal.listIterator();
+    BigDecimal bigDecimalResellerCost = new BigDecimal(0.0);
+    bigDecimalResellerCost.setScale(2, BigDecimal.ROUND_UP);
+    while (iterator.hasNext()) {
+
+	RawFilePojo obj = (RawFilePojo)iterator.next();
+
+	bigDecimalResellerCost = bigDecimalResellerCost.add(new BigDecimal(obj.getResellerCost().toString()));
+
+	System.out.println(bigDecimalResellerCost);
+
+}
+
+return bigDecimalResellerCost.toString();
+
+	    }
+
+	    }
--- ReconAutomation/src/main/java/Recon/ReconAutomation/raw/RawFileProcessing.java
+++ ReconAutomation/src/main/java/Recon/ReconAutomation/raw/RawFileProcessing.java
@@ -1,15 +1,19 @@
+package Recon.ReconAutomation.raw;
 
 import java.io.File;
-
 import java.io.FileInputStream;
+import java.math.BigDecimal;
 
+import Recon.ReconAutomation.raw.*;
 import java.util.ArrayList;
 
 import java.util.HashMap;
 
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
-
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -20,6 +24,7 @@ import Recon.ReconAutomation.raw.RawFilePojo;
 
 
 public class RawFileProcessing{
+private static final int RawFilePojo = 0;
 private String url;
 XSSFWorkbook workbook;
 public RawFileProcessing() {
@@ -32,33 +37,67 @@ public RawFileProcessing(String x) {
 x=this.url;
 }
 
-public void readFile(String ur) {
-String sheetName = "AP Recon";
-XSSFWorkbook workbook;
-try {
-			FileInputStream fileInputStreamRawFile=new FileInputStream(new File(ur));
-			workbook = new XSSFWorkbook(fileInputStreamRawFile);
-			System.out.println("File:" + workbook.getSheetIndex(sheetName));
-			XSSFSheet sheet = workbook.getSheetAt(0);
-			ArrayList<RawFilePojo> rawdataList = new ArrayList<RawFilePojo>();
-			for(int i=sheet.getFirstRowNum()+1;i<=sheet.getLastRowNum();i++) {
-			RawFilePojo e= new RawFilePojo();
-			rawdataList.add(e);
+public String readFile(String ur) {
+	String sheetName = "AP Recon";
+	XSSFWorkbook workbook;
+	try {
+		FileInputStream fileInputStreamRawFile=new FileInputStream(new File(ur));
+		workbook = new XSSFWorkbook(fileInputStreamRawFile);
+		System.out.println("File:" + workbook.getSheetIndex(sheetName));
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		List<RawFilePojo> rawdataList = new ArrayList<RawFilePojo>();
+		Map<String, List<RawFilePojo>> rawmapforpojo = new HashMap<String, List<RawFilePojo>>();
+		List<RawFilePojo> rawfilePojoList = new ArrayList<RawFilePojo>();
+		Iterator<Row> rowIterator = sheet.iterator();
+		while (rowIterator.hasNext()) {
+			Row row = rowIterator.next();
+			if(row.getRowNum()==0) {
+
 			}
-int x=0;
-for(RawFilePojo emp: rawdataList){
-x++;
-System.out.println("ID:"+emp.getSubscriptionid()+" firstName:"+emp.getPosttaxtotal());
-}
-System.out.println(x);
-fileInputStreamRawFile.close();
-}
-catch(Exception e) {
-e.printStackTrace();                
+			if(row.getRowNum()>0) {
+				RawFilePojo rawfilepojo = new RawFilePojo();
+				if(row.getCell(9)!=null) {
+					BigDecimal a = new BigDecimal(row.getCell(26).getNumericCellValue()*1.0941);
+					@SuppressWarnings("deprecation")
+					BigDecimal roundOff = a.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+					rawfilepojo = new RawFilePojo(row.getCell(9).toString(), row.getCell(26).toString());
+					rawfilepojo.setResellerCost(a.toString());
+					if(rawmapforpojo.containsKey(row.getCell(9).toString())) {
+						rawfilePojoList = rawmapforpojo.get(row.getCell(9).toString());
+						rawfilePojoList.add(rawfilepojo);
+					}else {
+						rawfilePojoList = new ArrayList<RawFilePojo>();
+						rawfilePojoList.add(rawfilepojo);
+						rawmapforpojo.put(row.getCell(9).toString(), rawfilePojoList);
+					}
+					System.out.println("Subscriptionid:" +rawfilepojo.getSubscriptionid()+"---"+ "Posttaxtotal:"+rawfilepojo.getPosttaxtotal()+"---"+"Resellercost:" +rawfilepojo.getResellerCost());
+				}
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//System.out.println(rawmapforpojo);
+		
+		fileInputStreamRawFile.close();
+	}
+		
+		
+		
+		
+		
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+	return sheetName;
 }
 }
 
 
-public class RawFileProcessing {
-
-}

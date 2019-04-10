85b2c15049fba8647e4ba2416800e41a56f8002b
Tanusree Chakraborty <Tanusree Chakraborty@TANUSREE_CHAKRABORTY>
2019-04-10 12:25:27 +0530

    startauto commit

--- ReconAutomation/src/main/java/Recon/ReconAutomation/StartAuto.java
+++ ReconAutomation/src/main/java/Recon/ReconAutomation/StartAuto.java
@@ -1,46 +1,37 @@
 package Recon.ReconAutomation;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import Recon.ReconAutomation.cmp.CmpFileProcessing;
+import Recon.ReconAutomation.raw.RawFilePojo;
+import Recon.ReconAutomation.raw.RawFileProcessed;
+//import Recon.ReconAutomation.raw.RawFileProcessed;
+import Recon.ReconAutomation.raw.RawFileProcessing;
 
 public class StartAuto {
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
-<<<<<<< HEAD
-		
-		String url = "F:\\Raw\\new.xlsx";
-
-		System.out.println("The directory to read the RAW files is - : " + url);
-
-		/*
-		 *  CmpFileProcessing cmpFileProcessing = new CmpFileProcessing(url);
-		 * 
-		 * 
-		 * cmpFileProcessing.readFile(url);
-		 */
-
-	/*System.out.println("Keep Calm and Code");
-=======
-		String url = "E:\\New folder\\a.xlsm";
-		System.out.println("The directory to read the RAW files is - : " + url); 
-		//System.out.println("The directory to read the CMP files is - : " + args[1]);
-		/*
-		 * System.out.println("The Country name is - : " + args[2]);
-		 * System.out.println("The Continent - : " + args[3]);
-		 */
-	/*	
-		CmpFileProcessing cmpFileProcessing = new CmpFileProcessing(url);
-		/* cmpFileProcessing.readFile(new File(args[0]), args[2], args[3]); */
-		//String path= args[0];
-		//cmpFileProcessing.readFile(url); 
-		
-
-		RawFileProcessing rawFileProcessing = new RawFileProcessing(url);
-		/* cmpFileProcessing.readFile(new File(args[0]), args[2], args[3]); */
-		//String path= args[0];
-		rawFileProcessing.readFile(url); 
+		/*	String url = "E:\\New folder\\testx.csv";
+		System.out.println("The directory to read the CMP files is - : " + url);
+		String urlcmp = "F:\\Raw\\new.xlsx";
+		CmpFileProcessing cmp = new CmpFileProcessing();
+		cmp.readFile(urlcmp);*/
+		String urlraw ="F:\\Raw\\new.xlsx";
+		System.out.println("The directory to read the RAW files is - : " + urlraw);
+		RawFileProcessing rawFileProcessing = new RawFileProcessing();
+		rawFileProcessing.readFile(urlraw); 
 		System.out.println("Keep Calm and Code");
->>>>>>> branch 'master' of https://github.com/AmitabhaDazz/Automation.git
-	}
 
+
+		RawFileProcessed processedObj = new RawFileProcessed(rawFileProcessing.getRawFilePojoList());
+		processedObj.createMapBySubscriptionID();
+
+		for (Map.Entry<String, List<RawFilePojo>> entry : processedObj.getSubscriptionIDMap().entrySet()) { 
+			String subId= entry.getKey();
+			System.out.println("For "  + subId + ", ResellerCostTotal is = " +  processedObj.getResellerCostBySubscriptionID(subId));
+			}
+	}
 }
